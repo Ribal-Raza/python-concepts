c88fe2e5cf211573440fefbb93f08b62603b8fb1
Ribal Raza <ribalraza1@gmail.com>
2024-01-26 17:52:56 +0000

    rps game updated

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,5 +1,6 @@
 import sys
 import random
+from enum import Enum
 
 # input() takes a string argument and prints it in cli, then waits for user input.
 # It takes the value of user input and then assigns it to variable it has been set to
@@ -7,25 +8,44 @@ import random
 # print(value)
 
 # Creating a game rock paper scissors
-print("")
-playerschoice = input("Enter... \n1 for Rock⛰, \n2 for Paper📃, \n3 for Scissors✂\n")
-player = int(playerschoice)  
 
-if player < 1 or player > 3:
-    sys.exit(print("Please chose from 1 to 3 "))
 
-computerchoice = random.choice("123")
-computer = int(computerchoice)
-print("You chose " + playerschoice + ".")
-print("Python chose " + computerchoice + ".")
+class RPS(Enum):
+    ROCK = 1
+    PAPER = 2
+    SCISSORS = 3
 
-if player == 1 and computer==3:
-    print("You Win! 🏆")
-elif player==2 and computer==1:
-    print("You Win! 🏆")
-elif player==3 and computer==2:
-     print("You Win! 🏆")
-elif player==computer:
-    print("Game Tie!👍")
-else:
-    print("Python🐍 Wins! 🏆")
+
+playagain = True
+
+while playagain:
+    print("")
+    playerschoice = input("Enter... \n1 for Rock⛰, \n2 for Paper📃, \n3 for Scissors✂\n")
+    player = int(playerschoice)
+    if player < 1 or player > 3:
+        sys.exit(print("Please chose from 1 to 3 "))
+
+    computerchoice = random.choice("123")
+    computer = int(computerchoice)
+    print("You chose " + playerschoice + ".")
+    print("Python chose " + computerchoice + ".")
+
+    if player == 1 and computer == 3:
+        print("You Win! 🏆")
+    elif player == 2 and computer == 1:
+        print("You Win! 🏆")
+    elif player == 3 and computer == 2:
+        print("You Win! 🏆")
+    elif player == computer:
+        print("Game Tie!👍")
+    else:
+        print("Python🐍 Wins! 🏆")
+
+    playagain = input("Do you want to play again? Y/N \t")
+    if playagain == "Y" or playagain == 'y':
+        continue
+    elif playagain == "N" or playagain=='n':
+        print("See you next time!👋")
+        playagain=False
+
+    
